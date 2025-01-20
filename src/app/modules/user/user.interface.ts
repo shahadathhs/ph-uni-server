35d217aa43fafc0029b7a7938f06bdd5aea4a544
shahadathhs <shahadathhossensajib732@gmail.com>
2025-01-20 21:23:16 +0600

    feat: updated middlewares

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,3 +1,4 @@
+import { USER_ROLE } from '../user/user.constant'
 export type TUser = {
   id: string
   password: string
@@ -6,3 +7,5 @@ export type TUser = {
   status: 'in-progress' | 'blocked'
   isDeleted: boolean
 }
+
+export type TUserRole = keyof typeof USER_ROLE
